6ece010cecee407cff05c877bd95070064eed02f
Jay Callicott <jrcallicott@gmail.com>
2024-11-18 10:10:13 -0600

    Added script to make sure sub_menu doesn't error if empty

---
+++ web/themes/custom/fws_raf/js/script-raf.js
@@ -0,0 +1,8 @@
+(function ($, Drupal, once) {
+
+  'use strict';
+
+  // Initialize the fws_submenu behavior if it doesn't exist
+  Drupal.behaviors.fws_submenu = Drupal.behaviors.fws_submenu || {};
+
+})(jQuery, Drupal, once);
