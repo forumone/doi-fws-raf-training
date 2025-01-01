a5ece95e0d3792b1bf84c80c3b2896860b9bf470
Jay Callicott <jrcallicott@gmail.com>
2025-01-01 09:05:09 -0600

    FWDV02-47: Refactored captives to be more memory efficient

--- web/modules/custom/tracking_reports/js/tracking_filter.js
+++ web/modules/custom/tracking_reports/js/tracking_filter.js
@@ -1,32 +1,11 @@
 (function ($, Drupal, once) {
   'use strict';
 
-  Drupal.behaviors.trackingFilter = {
+  Drupal.behaviors.facilityFilter = {
     attach: function (context, settings) {
-      once('trackingFilter', '.facility-filter', context).forEach(function (element) {
-        $(element).on('change', function () {
-          const selectedFacility = $(this).val();
-          const rows = $('.tracking-report-table tbody tr');
-          const tbody = $('.tracking-report-table tbody');
-          let visibleRows = 0;
-
-          // Remove existing no results message if it exists
-          $('.no-results-message').remove();
-
-          rows.each(function () {
-            if (selectedFacility === 'all' || $(this).attr('data-facility') === selectedFacility) {
-              $(this).show();
-              visibleRows++;
-            } else {
-              $(this).hide();
-            }
-          });
-
-          // Show message if no rows are visible
-          if (visibleRows === 0) {
-            const messageRow = $('<tr class="no-results-message"><td colspan="7">No results found for this organization</td></tr>');
-            tbody.append(messageRow);
-          }
+      once('facility-filter', '#facility-filter-select', context).forEach(function (element) {
+        $(element).on('change', function() {
+          $(this).closest('form').submit();
         });
       });
     }
---
+++ web/modules/custom/tracking_reports/src/Form/FacilityFilterForm.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Drupal\tracking_reports\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * A simple form to filter Current Captives by facility using GET params.
+ */
+class FacilityFilterForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'facility_filter_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Read the currently selected facility from ?facility=___ in the URL.
+    $selected_facility = \Drupal::request()->query->get('facility', 'all');
+
+    // Build facility options. In your real code, load the taxonomy terms from
+    // 'org'.
+    $facility_options = ['all' => $this->t('- All Facilities -')];
+
+    $storage = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
+    $org_terms = $storage->loadByProperties(['vid' => 'org']);
+    foreach ($org_terms as $term) {
+      if ($term->hasField('field_organization') && !$term->field_organization->isEmpty()) {
+        $org_val = $term->field_organization->value;
+        $facility_options[$org_val] = $org_val;
+      }
+    }
+
+    // We want GET submission so it appends ?facility= to the URL.
+    $form['#method'] = 'get';
+
+    // Build a select field that auto-submits on change.
+    $form['facility'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Filter by Facility'),
+      '#options' => $facility_options,
+      '#default_value' => $selected_facility,
+      '#attributes' => [
+        // When the user changes the select value, auto-submit.
+        'onchange' => 'this.form.submit();',
+      ],
+    ];
+
+    // We still need a "submit" element for the form to be valid, but we can
+    // hide it to avoid confusion.
+    $form['actions'] = [
+      '#type' => 'actions',
+      'submit' => [
+        '#type' => 'submit',
+        '#value' => $this->t('Apply Filter'),
+        '#attributes' => [
+          'style' => 'display: none;',
+        ],
+      ],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // Typically no validation needed for a simple select filter.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // On submit, redirect to the same page with ?facility=[value].
+    $selected = $form_state->getValue('facility', 'all');
+    $form_state->setRedirect('<current>', [], ['query' => ['facility' => $selected]]);
+  }
+
+}
