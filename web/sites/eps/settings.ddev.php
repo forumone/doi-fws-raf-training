30b9635955a9263c4ec697464ebafe8253175648
Jay Callicott <jrcallicott@gmail.com>
2025-01-16 12:11:44 -0600

    FWDV02-209: added Acquia configuration

---
+++ web/sites/eps/settings.ddev.php
@@ -0,0 +1,13 @@
+<?php
+
+$databases['default']['default'] = [
+  'database' => 'eps',
+  'username' => 'db',
+  'password' => 'db',
+  'host' => 'db',
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+];
+
+$settings['hash_salt'] = 'some-unique-hash-string';
