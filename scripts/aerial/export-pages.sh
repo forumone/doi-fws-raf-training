2a97b8da8cf4aa1007061d80d842ab88e8a20e77
Jay Callicott <jrcallicott@gmail.com>
2025-02-05 15:42:24 -0600

    Fixed homepage

--- scripts/aerial/export-pages.sh
+++ scripts/aerial/export-pages.sh
@@ -3,3 +3,4 @@
 ddev drush --uri=https://aerial.ddev.site/ dcer node 1 --folder=../recipes/fws-aerial-content/content
 ddev drush --uri=https://aerial.ddev.site/ dcer node 2 --folder=../recipes/fws-aerial-content/content
 ddev drush --uri=https://aerial.ddev.site/ dcer node 3 --folder=../recipes/fws-aerial-content/content
+ddev drush --uri=https://aerial.ddev.site/ dcer node 4 --folder=../recipes/fws-aerial-content/content
