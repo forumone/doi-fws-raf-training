86a2d52004a18d9cd02e0c90e0ec8279352ac470
Jay Callicott <jrcallicott@gmail.com>
2025-02-05 13:55:29 -0600

    FWDV02-254: added homepage

---
+++ scripts/aerial/export-pages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ddev drush --uri=https://aerial.ddev.site/ dcer node 1 --folder=../recipes/fws-aerial-content/content
+ddev drush --uri=https://aerial.ddev.site/ dcer node 2 --folder=../recipes/fws-aerial-content/content
+ddev drush --uri=https://aerial.ddev.site/ dcer node 3 --folder=../recipes/fws-aerial-content/content
