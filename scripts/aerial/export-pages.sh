2edf7cd35deb7b9be53562b8b4112773deb4e37e
Jay Callicott <jrcallicott@gmail.com>
2025-02-18 15:42:52 -0600

    FWDV02-145: Added history page and subpages with pdfs

--- scripts/aerial/export-pages.sh
+++ scripts/aerial/export-pages.sh
@@ -7,3 +7,6 @@ ddev drush --uri=https://aerial.ddev.site/ dcer node 4 --folder=../recipes/fws-a
 ddev drush --uri=https://aerial.ddev.site/ dcer node 5 --folder=../recipes/fws-aerial-content/content
 ddev drush --uri=https://aerial.ddev.site/ dcer node 6 --folder=../recipes/fws-aerial-content/content
 ddev drush --uri=https://aerial.ddev.site/ dcer node 7 --folder=../recipes/fws-aerial-content/content
+ddev drush --uri=https://aerial.ddev.site/ dcer node 8 --folder=../recipes/fws-aerial-content/content
+ddev drush --uri=https://aerial.ddev.site/ dcer node 9 --folder=../recipes/fws-aerial-content/content
+ddev drush --uri=https://aerial.ddev.site/ dcer node 10 --folder=../recipes/fws-aerial-content/content
