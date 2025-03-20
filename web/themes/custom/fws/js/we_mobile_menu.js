4ca7fd4af88f8029e9070d2bd6495f3970f15b86
Eric Shell <ericjshell@gmail.com>
2025-03-20 13:37:55 -0700

    FWDV02-268: Updating all mobile menu functionality for better compliance

--- web/themes/custom/fws/js/we_mobile_menu.js
+++ web/themes/custom/fws/js/we_mobile_menu.js
@@ -58,12 +58,9 @@
           $(settings.targetWrapper).addClass('mobile-main-menu');
         } else {
           $(settings.targetWrapper).removeClass('mobile-main-menu');
-          $('body').css('overflow', '');
-          $('body').css('height', '');
-          $('body').css('position', '');
+          $(settings.pageSelector).removeClass('mobile-menu-open');
           $(settings.pageSelector).removeClass(settings.toggledClass);
           $(settings.pageSelector).find('.overlay').remove();
-          $(settings.pageSelector).css('position', '');
           item.removeClass('open');
           item.find('ul').css('display', '');
           $(document).off('keydown', trapFocus);
@@ -75,11 +72,8 @@
         var wrapper = $(settings.pageSelector);
         var overlay = wrapper.find('.overlay');
         overlay.remove();
-        wrapper.css({
-          'width': '',
-          'position': ''
-        });
         wrapper.removeClass(settings.toggledClass);
+        wrapper.removeClass('mobile-menu-open');
         wrapper.find('div.region-we-mega-menu nav').removeClass('we-mobile-megamenu-active');
         $(document).off('keydown', trapFocus);
         $('#navbar-collapse').attr('aria-expanded', 'false');
@@ -87,44 +81,28 @@
         if (overlay.length > 0) {
           wrapper.find('.btn-close').remove();
           overlay.remove();
-          $('body').css('overflow', '');
-          $('body').css('height', '');
-          $('body').css('position', '');
         }
       }
 
       this.off('click.mobileMenu');
       this.on('click.mobileMenu', function (e) {
-        /* START CUSTOMIZATION
-        * this line needs to get the main mega menu, and the orginal source was designed
-        * to work with multiple mega menus, so it would take the original we mega menu hamburger
-        * (the item clicked) and find the closest mega menu. Since we only have one and changed
-        * the clickable item to the bootstrap hamburger, this line needed editing to get the
-        * right menu.
-        * Orignal line
-        * var targetWrapper = $(this).closest('div.region-we-mega-menu').find('nav.navbar-we-mega-menu');
-        * */
         var targetWrapper = $('div.region-we-mega-menu').find('nav.navbar-we-mega-menu');
-        // END CUSTOMIZATION
 
         var wrapper = $(settings.pageSelector);
         if (!wrapper.hasClass(settings.toggledClass)) {
-          wrapper.addClass(settings.toggledClass).css('position', 'relative');
+          wrapper.addClass(settings.toggledClass);
+          wrapper.addClass('mobile-menu-open');
           $(settings.targetWrapper).addClass('mobile-main-menu');
           targetWrapper.addClass('we-mobile-megamenu-active');
           $(document).on('keydown', trapFocus);
           $('#navbar-collapse').attr('aria-expanded', 'true');
 
           if (wrapper.find('.overlay').length == 0) {
-            var overlay = $('<div class="overlay"></div>');
+            var overlay = $('<div class="overlay mobile-menu-overlay"></div>');
             overlay.prependTo(wrapper);
             overlay.click(function () {
               _weMegaMenuClear();
             });
-            $('body').css('overflow', 'hidden');
-            $('body').css('btn-close', 'hidden');
-            $('body').css('height', '100%');
-            $('body').css('position', 'relative');
           }
           if (wrapper.find('.btn-close').length == 0) {
             var btnClose = $('<button class="btn-close menu__toggle"></button>');
@@ -132,15 +110,8 @@
 
             $('.btn-close').on('click', function (e) {
               _weMegaMenuClear();
-              /* START CUSTOMIZATION
-              * Since the bootstrap button is opening both the region-collapsible and the mega menu
-              * when the close button is closed, it needs to close the region-collapsible as well.
-              * so this will toggle that class to do so.
-              */
               $("#navbar-collapse").removeClass('in');
-              //this gives the header a darker gradient, remove it when menu is closed.
-              $('body').removeClass('mega-menu-open')
-              // END CUSTOMIZATION
+              $('body').removeClass('mega-menu-open');
               e.preventDefault();
               return false;
             });
@@ -149,13 +120,6 @@
           _weMegaMenuClear();
         }
         e.preventDefault();
-        /* START CUSTOMIZATION
-        * Since the bootstrap button is opening both the region-collapsible and the mega menu
-        * we cannot allow propagation to stop, we need to allow it to continue so bootstrap will
-        * open up region-collaspisble. So i commented this line out.
-        */
-        //e.stopPropagation();
-        /* END CUSTOMIZATION */
       });
 
       if (settings.accordionMenu == 'true') {
